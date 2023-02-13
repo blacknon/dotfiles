127798ca23c19d97c8f02d396c9a589aa9f1b6d7
blacknon <blacknon@orebibou.com>
2023-02-14 00:18:17 +0900

    update

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Copyright(c) 2023 Blacknon. All rights reserved.
 # Use of this source code is governed by an MIT license
 # that can be found in the LICENSE file.
